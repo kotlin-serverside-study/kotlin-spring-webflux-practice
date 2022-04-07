e9ab9a0bf6790a2d47cc58acc730db4cf04be8a5
humanitas03 <dlswp113@gmail.com>
2022-04-07 23:47:26 +0900

    init project

---
+++ src/test/kotlin/com/example/kopring/webflux/ProjectApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.kopring.webflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
